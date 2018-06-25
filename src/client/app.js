4838128ad35d4edb7bb44a4180e87e8765129a1b
Rob Bruhn <rob.bruhn@raise.com>
2018-06-25 11:04:42 -0500

    Remove css modules

--- src/client/app.js
+++ src/client/app.js
@@ -1,14 +1,14 @@
 // @flow
 import React, { Component } from "react";
 import fetchListings from "../server/fetchListings";
-import styles from "./index.module.css";
+import "./index.css";
 
 class App extends Component<*, *> {
   render() {
     return (
-      <div className={styles.app}>
+      <div className="app">
         <h1>Live Coding Challenge</h1>
-        <div className={styles.content}>
+        <div className="content">
           <h2>Listings</h2>
           <h2>Cart</h2>
         </div>
